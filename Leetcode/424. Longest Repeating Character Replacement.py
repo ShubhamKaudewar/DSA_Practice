51e0892a976161b0d725830ef5d5464840e7b25f
Shubham Kaudewar <you@example.com>
2024-12-29 14:37:23 +0530

    solved 424 and 689

--- Leetcode/424. Longest Repeating Character Replacement.py
+++ Leetcode/424. Longest Repeating Character Replacement.py
@@ -1,8 +1,19 @@
+from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        ...
+        res = 0
+        count = defaultdict(lambda: 0)
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] += 1
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            res = max(res, r - l + 1)
+        return res
 
-sol = Solution()
-s = "ABAB"
-k = 2
-print(sol.characterReplacement(s, k))
+if __name__ == '__main__':
+    sol = Solution()
+    s = "ABAB"
+    k = 2
+    print(sol.characterReplacement(s, k))
