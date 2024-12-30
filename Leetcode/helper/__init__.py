58e3c62ff745c07c3f3c4d3f80d0bc1465b33aaa
Shubham Kaudewar <you@example.com>
2024-12-30 14:15:17 +0530

    tree level order traversal practiced again

---
+++ Leetcode/helper/__init__.py
@@ -0,0 +1,1 @@
+from .construct_binary_tree import TreeNode
---
+++ Leetcode/helper/construct_binary_tree.py
@@ -0,0 +1,27 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def insert_level_order(self, arr, root, i, n):
+        if i < n:
+            temp = TreeNode(arr[i]) if arr[i] is not None else None
+            root = temp
+
+            if temp is not None:
+                root.left = self.insert_level_order(arr, root.left, 2 * i + 1, n)
+                root.right = self.insert_level_order(arr, root.right, 2 * i + 2, n)
+        return root
+
+    def construct_binary_tree(self, arr):
+        return self.insert_level_order(arr, None, 0, len(arr))
+
+
+
+if __name__ == '__main__':
+    # Example usage
+    tree = TreeNode()
+    arr = [3, 9, 20, None, None, 15, 7]
+    root = tree.construct_binary_tree(arr)
+    print("==== END ====")
