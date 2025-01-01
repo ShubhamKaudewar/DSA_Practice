1c9c3f5bc7e0050a92b88f3ad9d779965f695dce
Shubham Kaudewar <you@example.com>
2025-01-01 19:31:55 +0530

    the one with complex prefix question

--- Leetcode/560. Subarray Sum Equals K.py
+++ Leetcode/560. Subarray Sum Equals K.py
@@ -3,6 +3,28 @@ from typing import List
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        total = 0
+        d = {}
+        counter = 0
+
+        for i in range(len(nums)):
+            total += nums[i]
+            if total == k:
+                counter += 1
+
+            if (total - k) in d:
+                counter += d[total - k]
+
+            if total in d:
+                d[total] += 1
+            else:
+                d[total] = 1
+        return counter
+
+
+
+    # Less efficient time complexity O(n)
+    def subarraySum1(self, nums: List[int], k: int) -> int:
         k_sum = [0] * (len(nums) + 1)
         for idx, num in enumerate(nums):
             k_sum[idx+1] = k_sum[idx] + num
