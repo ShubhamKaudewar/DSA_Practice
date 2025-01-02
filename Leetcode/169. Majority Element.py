89e85e2d6d5c0b634cc5b3e818bbefec784f45dc
Shubham Kaudewar <you@example.com>
2025-01-02 14:26:32 +0530

    solved majority element 2

--- Leetcode/169. Majority Element.py
+++ Leetcode/169. Majority Element.py
@@ -3,6 +3,12 @@ from collections import Counter
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        """
+        Here we are checking the count of the candidate number. Only for 1 possible solution for to be
+        majority element. Which is frequency of the candidate number > n/2
+        In majority element II problem, we are checking frequency of all the numbers > n/2. Which are possible
+        solution are 2 candidates. So we will initialize two candidates there
+        """
         candidate = 0
         count = 0
         for num in nums:
