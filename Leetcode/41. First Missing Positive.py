af8848319988d45cd675094262b2a880856fcca5
Shubham Kaudewar <you@example.com>
2025-01-03 22:27:39 +0530

    this question require cyclic sorting and index swaping concept

--- Leetcode/41. First Missing Positive.py
+++ Leetcode/41. First Missing Positive.py
@@ -13,9 +13,7 @@ class Solution:
             elif not nums[i]:
                 i += 1
             else:
-                t = nums[nums[i] - 1]
-                nums[nums[i] - 1] = nums[i]
-                nums[i] = t
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
             if i == n-1:
                 break
 
@@ -24,11 +22,33 @@ class Solution:
                 return idx+1
         return n
 
+    """
+    Time Complexity: O(n)
+    This is more optimized use cyclic sort and index swap. 
+    This code will pass 1 edge case for which above code failing as TLE
+    """
+    def firstMissingPositive2(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        for i in range(n):
+            # Place nums[i] in its correct position if possible
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                # Swap nums[i] with nums[nums[i] - 1]
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+
+        # Find the first index where the number is not in the correct position
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+
+        # If all numbers are in the correct position, return n + 1
+        return n + 1
+
 if __name__ == '__main__':
     sol = Solution()
-    nums = [1,2,0]
-    print(sol.firstMissingPositive(nums) == 3)
+    # nums = [1,2,0]
+    # print(sol.firstMissingPositive2(nums) == 3)
     nums = [3,4,-1,1]
-    print(sol.firstMissingPositive(nums) == 2)
+    print(sol.firstMissingPositive2(nums) == 2)
     nums = [7,8,9,11,12]
-    print(sol.firstMissingPositive(nums) == 1)
+    print(sol.firstMissingPositive2(nums) == 1)
