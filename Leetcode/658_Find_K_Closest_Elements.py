5187a261536ec5a1f6a4457611b81698e1e4b1d7
Shubham Kaudewar <you@example.com>
2025-01-04 02:44:28 +0530

    solved using bisect and max and min heap

--- Leetcode/658_Find_K_Closest_Elements.py
+++ Leetcode/658_Find_K_Closest_Elements.py
@@ -1,19 +1,49 @@
 from typing import List
-from collections import Counter
+import pytest
+import bisect
+import heapq
 
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        s1_map = Counter(s1)
-        s1_len = len(s1)
+        idx = bisect.bisect_left(arr, x)
 
-        for head in range(s1_len-1, len(s2)):
-            if s2[head] in s1_map:
-                sub_s = s2[head-s1_len+1:head+1]
-                if Counter(sub_s) == s1_map:
-                    return True
-        return False
+        res = []
+        right_heap = arr[idx:]
+        heapq.heapify(right_heap)
+        left_heap = arr[:idx]
+        heapq._heapify_max(left_heap)
+        while k > 0:
+            if left_heap and right_heap:
+                left_peek = left_heap[0]
+                right_peek = right_heap[0]
+                if abs(left_peek - x) <= abs(right_peek - x):
+                    res.append(heapq._heappop_max(left_heap))
+                else:
+                    res.append(heapq.heappop(right_heap))
+            elif left_heap:
+                res.append(heapq._heappop_max(left_heap))
+            elif right_heap:
+                res.append(heapq.heappop(right_heap))
+            else:
+                return sorted(res)
+            k -= 1
+        return sorted(res)
+
+def test_case_1():
+    sol = Solution()
+    assert sol.findClosestElements([1, 2, 3, 4, 5], 4, 3) == [1, 2, 3, 4]
+
+def test_case_2():
+    sol = Solution()
+    assert sol.findClosestElements([1, 1, 2, 3, 4, 5], 4, -1) == [1, 1, 2, 3]
+
+def test_case_3():
+    sol = Solution()
+    assert sol.findClosestElements([0,1,1,1,2,3,6,7,8,9], 9, 4) == [0,1,1,1,2,3,6,7,8]
+
+def test_case_4():
+    sol = Solution()
+    assert sol.findClosestElements([0,1,2,2,2,3,6,8,8,9], 5, 9) == [3,6,8,8,9]
 
 if __name__ == '__main__':
-    sol = Solution()
-    arr, k, x, answer = [1,2,3,4,5], 4, 3, [1,2,3,4]
-    print(sol.findClosestElements(arr, k, x) == answer)
+    pytest.main()
